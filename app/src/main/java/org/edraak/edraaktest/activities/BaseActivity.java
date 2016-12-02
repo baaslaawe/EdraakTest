80ccd12b90667e78779d5d7456270d0b2494fc00
Bilal Sammour <bilalsammour@gmail.com>
2016-12-02 10:40:13 +0200

    organize activities

---
+++ app/src/main/java/org/edraak/edraaktest/activities/BaseActivity.java
@@ -0,0 +1,11 @@
+package org.edraak.edraaktest.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * The top parent for all activities
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract void initViews();
+}
