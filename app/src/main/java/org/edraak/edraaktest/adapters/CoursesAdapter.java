ff8968dbc2ba45efc1b6cd7c7798b4b8a7fd955d
Bilal Sammour <bilalsammour@gmail.com>
2016-12-02 16:11:16 +0200

    adding courses adapter for recycler view

---
+++ app/src/main/java/org/edraak/edraaktest/adapters/CoursesAdapter.java
@@ -0,0 +1,95 @@
+package org.edraak.edraaktest.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.edraak.edraaktest.R;
+import org.edraak.edraaktest.models.thin.CourseModel;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+/**
+ * Courses adapter
+ */
+public class CoursesAdapter extends BaseListAdapter
+        <CoursesAdapter.CourseViewHolder, CourseModel> {
+
+    private Context context;
+
+    /**
+     * Create an instance
+     *
+     * @param context a context of the application package implementing
+     *                this class.
+     */
+    public CoursesAdapter(Context context) {
+        super();
+
+        this.context = context;
+    }
+
+    @Override
+    protected CourseViewHolder onCreateRegularViewHolder
+            (ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_course, parent, false);
+
+        return new CourseViewHolder(view);
+    }
+
+    @Override
+    protected void onRegularBindViewHolder
+            (CourseViewHolder holder, int position, CourseModel item) {
+        setValueNumberWithLabel(holder.getId(), item.getId(), R.string.id);
+        setValueNumberWithLabel(holder.getCourseId(),
+                item.getId(), R.string.course_id);
+        setValueNumberWithLabel(holder.getCourseCategoryId(),
+                item.getId(), R.string.course_category_id);
+    }
+
+    private void setValueNumberWithLabel
+            (TextView textView, long value, int labelResourceId) {
+        String label = context.getString(labelResourceId);
+        String fullText = String.format(Locale.getDefault(),
+                "%s: %d", label, value);
+
+        textView.setText(fullText);
+    }
+
+    @Override
+    protected void initItems() {
+        setList(new ArrayList<CourseModel>());
+    }
+
+    protected class CourseViewHolder extends
+            BaseListAdapter.ItemBaseViewHolder {
+
+        private TextView id;
+        private TextView courseId;
+        private TextView courseCategoryId;
+
+        public CourseViewHolder(View itemView) {
+            super(itemView);
+
+            id = (TextView) itemView.findViewById(R.id.id);
+            courseId = (TextView) itemView.findViewById(R.id.courseId);
+            courseCategoryId = (TextView) itemView.findViewById(R.id.courseCategoryId);
+        }
+
+        public TextView getId() {
+            return id;
+        }
+
+        public TextView getCourseId() {
+            return courseId;
+        }
+
+        public TextView getCourseCategoryId() {
+            return courseCategoryId;
+        }
+    }
+}
