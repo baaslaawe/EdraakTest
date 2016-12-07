57aebdee706227c5e31439b150220fd6adab05e9
Bilal Sammour <bilalsammour@gmail.com>
2016-12-07 10:35:56 +0200

    animate recycler view items

--- app/src/main/java/org/edraak/edraaktest/adapters/CoursesAdapter.java
+++ app/src/main/java/org/edraak/edraaktest/adapters/CoursesAdapter.java
@@ -1,9 +1,12 @@
 package org.edraak.edraaktest.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 import org.edraak.edraaktest.R;
@@ -11,6 +14,7 @@ import org.edraak.edraaktest.models.thin.CourseModel;
 
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Random;
 
 /**
  * Courses adapter
@@ -19,6 +23,8 @@ public class CoursesAdapter extends BaseListAdapter
         <CoursesAdapter.CourseViewHolder, CourseModel> {
 
     private Context context;
+
+    private int lastPosition = -1;
 
     /**
      * Create an instance
@@ -49,6 +55,8 @@ public class CoursesAdapter extends BaseListAdapter
                 item.getCourseId(), R.string.course_id);
         setValueNumberWithLabel(holder.getCourseCategoryId(),
                 item.getCourseCategoryId(), R.string.course_category_id);
+
+        animateView(holder.getContainer(), position);
     }
 
     private void setValueNumberWithLabel
@@ -60,35 +68,56 @@ public class CoursesAdapter extends BaseListAdapter
         textView.setText(fullText);
     }
 
+    private void animateView(View viewToAnimate, int position) {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition) {
+            ScaleAnimation anim =
+                    new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+                            Animation.RELATIVE_TO_SELF, 0.5f,
+                            Animation.RELATIVE_TO_SELF, 0.5f);
+            anim.setDuration(new Random()
+                    .nextInt(501));//to make duration random number between [0,501)
+            viewToAnimate.startAnimation(anim);
+
+            lastPosition = position;
+        }
+    }
+
     @Override
     protected void initItems() {
         setList(new ArrayList<CourseModel>());
     }
 
-    protected class CourseViewHolder extends
+    class CourseViewHolder extends
             BaseListAdapter.ItemBaseViewHolder {
 
+        private CardView container;
         private TextView id;
         private TextView courseId;
         private TextView courseCategoryId;
 
-        public CourseViewHolder(View itemView) {
+        CourseViewHolder(View itemView) {
             super(itemView);
 
+            container = (CardView) itemView.findViewById(R.id.container);
             id = (TextView) itemView.findViewById(R.id.id);
             courseId = (TextView) itemView.findViewById(R.id.courseId);
             courseCategoryId = (TextView) itemView.findViewById(R.id.courseCategoryId);
         }
 
-        public TextView getId() {
+        CardView getContainer() {
+            return container;
+        }
+
+        TextView getId() {
             return id;
         }
 
-        public TextView getCourseId() {
+        TextView getCourseId() {
             return courseId;
         }
 
-        public TextView getCourseCategoryId() {
+        TextView getCourseCategoryId() {
             return courseCategoryId;
         }
     }
